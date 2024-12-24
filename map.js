4fd065f931982696337310d18b9367c9d033c4c7
Adil Ali <106112362+Adil8081@users.noreply.github.com>
2024-12-25 00:22:19 +0530

    extractCoordinates

--- map.js
+++ map.js
@@ -2,9 +2,6 @@
 // Maintain the order of their first appearance in each string
 const uniqueCharactersOf = function (strings) {};
 
-// extract coordinates from [{ x: 1, y: 2 }, { x: 3, y: 4 }] => [[1, 2], [3, 4]]
-const extractCoordinates = function (objects) {};
-
 // extract full name and age from [{ firstName: "Alice", lastName: "Smith", age: 25 }, { firstName: "Bob", lastName: "Brown", age: 30 }] => [["Alice Smith", 25], ["Bob Brown", 30]]
 const fullNameAndAge = function (objects) {};
 
