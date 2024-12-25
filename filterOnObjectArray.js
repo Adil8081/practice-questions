bcb765272a2f0f717b8d00e003e8c1dfcb3fc6e8
Adil Ali <106112362+Adil8081@users.noreply.github.com>
2024-12-25 17:43:40 +0530

    moving object problem to filterOnObjectArray.js

---
+++ filterOnObjectArray.js
@@ -0,0 +1,79 @@
+// people older than 30 [{name: "Alice", age: 25}, {name: "Bob", age: 35}] => [{name: "Bob", age: 35}]
+const ageAbove30 = function (person) {
+  return person.age > 30;
+};
+
+const filterAdults = function (people) {
+  return people.filter(ageAbove30);
+};
+
+// console.log(filterAdults([{ name: "Alice", age: 25 }, { name: "Bob", age: 35 }]));
+
+// books with more than 200 pages [{title: "Book 1", pages: 150}, {title: "Book 2", pages: 250}] => [{title: "Book 2", pages: 250}]
+const hasAbove200Pages = function (book) {
+  return book.pages > 200;
+};
+
+const filterLongBooks = function (books) {
+  return books.filter(hasAbove200Pages);
+};
+
+// console.log(filterLongBooks([{ title: "Book 1", pages: 150 }, { title: "Book 2", pages: 250 }]));
+
+// students with grades above 80 [{name: "John", grade: 75}, {name: "Jane", grade: 85}] => [{name: "Jane", grade: 85}]
+const hasGradeAbove80 = function (student) {
+  return student.grade > 80;
+};
+
+const filterHighGrades = function (students) {
+  return students.filter(hasGradeAbove80);
+};
+
+// console.log(filterHighGrades([{ name: "John", grade: 75 }, { name: "Jane", grade: 85 }]));
+
+// orders placed in the last 30 days [{orderDate: "2024-11-01"}, {orderDate: "2024-12-01"}] => [{orderDate: "2024-12-01"}]
+const orderedWithin30Days = function (orderDetail) {
+  const date = orderDetail.orderDate.split("-");
+  const month = date[1];
+  const day = date.at(-1);
+
+  if ((day >= 22 && +month === 11) || +month === 12) {
+    return true;
+  }
+
+  return false;
+};
+
+const filterRecentOrders = function (orders) {
+  return orders.filter(orderedWithin30Days);
+};
+
+console.log(
+  filterRecentOrders([
+    { orderDate: "2024-11-01" },
+    { orderDate: "2024-12-01" },
+    { orderDate: "2024-11-24" },
+  ])
+);
+
+// products with a price lower than the average [{name: "item1", price: 10}, {name: "item2", price: 20}, {name: "item3", price: 5}] => [{name: "item1", price: 10}, {name: "item3", price: 5}]
+const computeAvgPrice = function (products) {
+  let totalPrice = 0;
+  const noOfProducts = products.length;
+
+  for (const product of products) {
+    totalPrice += product.price;
+  }
+
+  return totalPrice / noOfProducts;
+};
+
+const filterBelowAveragePrice = function (products) {
+  const averagePrice = computeAvgPrice(products);
+
+  return products.filter(function (product) {
+    return product.price < averagePrice;
+  });
+};
+
+// console.log(filterBelowAveragePrice([{ name: "item1", price: 10 }, { name: "item2", price: 20 }, { name: "item3", price: 5 }]));
