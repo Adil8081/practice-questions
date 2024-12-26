b99caef2cefca48e132dca1334b46af4af609847
Adil Ali <106112362+Adil8081@users.noreply.github.com>
2024-12-26 10:00:14 +0530

    moving reduce problem based on string array to reduceOnStringArray.js

--- reduce.js
+++ reduce.js
@@ -1,25 +1,3 @@
-// concatenateWords(["hello", "world"]) => "helloworld"
-const concatWord = function (string, word) {
-  return string.concat(word);
-};
-
-const concatenateWords = function (words) {
-  return words.reduce(concatWord, "");
-};
-
-// console.log(concatenateWords(["hello", "world"]));
-
-// longestWord(["apple", "banana", "cherry", "kiwi"]) => "banana"
-const longerWord = function (longWord, word) {
-  return word.length > longWord.length ? word : longWord;
-};
-
-const longestWord = function (words) {
-  return words.reduce(longerWord, "");
-};
-
-// console.log(longestWord(["apple", "banana", "cherry", "kiwi"]));
-
 // shortestWord(["apple", "banana", "cherry", "kiwi"]) => "kiwi"
 const shortestWord = function (words) {};
 
